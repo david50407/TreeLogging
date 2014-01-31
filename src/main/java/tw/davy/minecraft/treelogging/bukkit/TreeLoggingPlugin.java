7d84d4709bcf6427aedb210e163cd4b3178ecaae
David Kuo <s50407s@gmail.com>
2014-01-31 13:40:37 +0800

    Initial Commit

---
+++ src/main/java/tw/davy/minecraft/treelogging/bukkit/TreeLoggingPlugin.java
@@ -0,0 +1,13 @@
+package tw.davy.minecraft.treelogging.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The main class for TreeLoggingPlugin as a Bukkit plugin.
+ *
+ * @author Davy
+ */
+public final class TreeLoggingPlugin extends JavaPlugin
+{
+
+}
